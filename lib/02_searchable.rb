9c050c59e6111322509cc93d2ac666e2e40e82e7
adamberman <adamberman13@gmail.com>
2014-10-24 15:31:59 -0700

    create associations

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,10 +3,20 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.keys.map { |key| "#{key} = ?"}.join(' AND ')
+    values = params.values
+    query = <<-SQL
+    SELECT
+    	*
+    FROM
+    	#{table_name}
+    WHERE
+    	#{where_line}
+    SQL
+    parse_all(DBConnection.execute(query, *values))
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,34 +10,51 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] || (name.to_s + "Id").underscore.to_sym
+    @class_name = options[:class_name] || name.to_s.camelcase
+    @primary_key = options[:primary_key] || :id 
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @foreign_key = options[:foreign_key] || (self_class_name.to_s.singularize + "Id").underscore.to_sym
+    @class_name = options[:class_name] || name.to_s.camelcase.singularize
+    @primary_key = options[:primary_key] || :id 
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+    define_method "#{name}" do
+      foreign_key = send(options.foreign_key)
+      options
+        .model_class
+        .where(options.primary_key => foreign_key)
+        .first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, self, options)
+    define_method "#{name}" do
+      primary_key = send(options.primary_key)
+      options
+        .model_class
+        .where(options.foreign_key => primary_key)
+    end
   end
 
   def assoc_options
@@ -46,5 +63,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
